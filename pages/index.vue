89d05b04d530c759f4de93a0b67c16ac180ceaf0
高岩ゆりえ <takaiwa@tsumikiinc.com>
2020-03-24 18:27:36 +0900

    pugの導入

--- pages/index.vue
+++ pages/index.vue
@@ -1,31 +1,5 @@
-<template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        nuxt-files
-      </h1>
-      <h2 class="subtitle">
-        My funkadelic Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
+<template lang="pug">
+  h1 test
 </template>
 
 <script>
